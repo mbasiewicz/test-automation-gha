3d56cf2abb79f535dcba5064b395f3c42abc6222
Bartosz Woźniak <17177420+wozniakpl@users.noreply.github.com>
2023-06-29 22:40:51 +0200

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+
+def factorials(n):
+    factorial=1
+    for i in range(1, n + 1):
+        factorial *= i
+        yield factorial
+
+def main():
+    for i, factorial in enumerate(factorials(100)):
+        print(f"{i + 1}! = {factorial}")
+
+
+if __name__ == "__main__":
+    main()
