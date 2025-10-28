bb5caa9cbc10d0d61d858d87278196a26e4108a5
Marcin Basiewicz <m.basiewicz@tme.pl>
2025-10-28 20:16:32 +0100

    dodanie test job do CI workflow

---
+++ tests/test_factorials.py
@@ -0,0 +1,26 @@
+from main import factorials
+import pytest
+
+
+def test_positive_numbers():
+    """sprawdza poprawnosc funkcji przy liczbie dodatniej jako argument"""
+    result = list(factorials(5))
+    assert result == [1, 2, 6, 24, 120]
+
+
+def test_negative_numbers():
+    """sprawdza poprawnosc funkcji przy liczbie ujemnej jako argument"""
+    result = list(factorials(-3))
+    assert result == []
+
+
+def test_zero():
+    """sprawdza poprawnosc funkcji 0 jako argument"""
+    result = list(factorials(0))
+    assert result == []
+
+
+def test_float_numbers():
+    """sprawdza poprawnosc funkcji przy liczbie float jako argument"""
+    with pytest.raises(TypeError):
+        list(factorials(3.5))
