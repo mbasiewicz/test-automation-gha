7bba13428a268681685f2fbffeb34194ab9e56c1
Marcin Basiewicz <m.basiewicz@tme.pl>
2025-10-28 18:57:07 +0100

    poprawione formatowanie kodu

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import sys
-
 def factorials(n):
-    factorial=1
+    factorial = 1
     for i in range(1, n + 1):
         factorial *= i
         yield factorial
+
 
 def main():
     for i, factorial in enumerate(factorials(100)):
